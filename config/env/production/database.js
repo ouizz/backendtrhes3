a415c0ec7fb1b5a93e3804d39ab5cb633532b8c6
MAC70006498 <mac70006498@MAC70006498s-MacBook-Pro.local>
2024-09-22 00:12:03 +0700

    update

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,60 +1,21 @@
-const path = require('path');
+// path: ./config/env/production/database.ts
 
-module.exports = ({ env }) => {
-  const client = env('DATABASE_CLIENT', 'sqlite');
+import { parse } from 'pg-connection-string';
+const config = parse(process.env.DATABASE_URL);
 
-  const connections = {
-    mysql: {
-      connection: {
-        host: env('DATABASE_HOST', 'localhost'),
-        port: env.int('DATABASE_PORT', 3306),
-        database: env('DATABASE_NAME', 'strapi'),
-        user: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'strapi'),
-        ssl: env.bool('DATABASE_SSL', false) && {
-          key: env('DATABASE_SSL_KEY', undefined),
-          cert: env('DATABASE_SSL_CERT', undefined),
-          ca: env('DATABASE_SSL_CA', undefined),
-          capath: env('DATABASE_SSL_CAPATH', undefined),
-          cipher: env('DATABASE_SSL_CIPHER', undefined),
-          rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
-        },
-      },
-      pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
-    },
-    postgres: {
-      connection: {
-        connectionString: env('DATABASE_URL'),
-        host: env('DATABASE_HOST', 'localhost'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapi'),
-        user: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'strapi'),
-        ssl: env.bool('DATABASE_SSL', false) && {
-          key: env('DATABASE_SSL_KEY', undefined),
-          cert: env('DATABASE_SSL_CERT', undefined),
-          ca: env('DATABASE_SSL_CA', undefined),
-          capath: env('DATABASE_SSL_CAPATH', undefined),
-          cipher: env('DATABASE_SSL_CIPHER', undefined),
-          rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
-        },
-        schema: env('DATABASE_SCHEMA', 'public'),
-      },
-      pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
-    },
-    sqlite: {
-      connection: {
-        filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
-      },
-      useNullAsDefault: true,
-    },
-  };
-
-  return {
+export default ({ env }) => ({
+  connection: {
+    client: 'mysql',
     connection: {
-      client,
-      ...connections[client],
-      acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
+      host: config.DATABASE_HOST,
+      port: config.DATABASE_PORT,
+      database: config.DATABASE_NAME,
+      user: config.DATABASE_USERNAME,
+      password: config.DATABASE_PASSWORD,
+      ssl: {
+        rejectUnauthorized: false
+      },
     },
-  };
-};
+    debug: false,
+  },
+});
--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,11 +1,8 @@
-module.exports = ({ env }) => ({
-    host: env('HOST', '0.0.0.0'),
-    port: env.int('PORT', 1337),
-    app: {
-      keys: env.array('APP_KEYS'),
-    },
-    webhooks: {
-      populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
-    },
-  });
-  
+// Path: ./config/env/production/server.js`
+export default ({ env }) => ({
+    proxy: true,
+    url: env('MY_HEROKU_URL'), // Sets the public URL of the application.
+    app: { 
+         keys: env.array('APP_KEYS')
+       },
+   });
