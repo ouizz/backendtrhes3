40960c04c9d5ab9533faf9e90c50e9df0780356c
MAC70006498 <mac70006498@MAC70006498s-MacBook-Pro.local>
2024-09-22 00:13:56 +0700

    update gb

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -7,11 +7,11 @@ export default ({ env }) => ({
   connection: {
     client: 'mysql',
     connection: {
-      host: config.DATABASE_HOST,
-      port: config.DATABASE_PORT,
-      database: config.DATABASE_NAME,
-      user: config.DATABASE_USERNAME,
-      password: config.DATABASE_PASSWORD,
+      host: 'ls-8032805010d2fe7921cb2c79a3408c75c45a60b1.c9koqwe84rkw.ap-southeast-1.rds.amazonaws.com',
+      port: '3306',
+      database: 'httpstha_thekaka',
+      user: 'dbmasteruser',
+      password: 'HnbtvWSqEDPGUYC2Mgpjxk',
       ssl: {
         rejectUnauthorized: false
       },
